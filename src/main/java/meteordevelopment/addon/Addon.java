f1702c8df32449a78f3306698bb5e6fe89d9d540
Gambl86 <Philipp4124@gmail.com>
2026-07-14 16:49:40 +0200

    Update Addon.java

--- src/main/java/meteordevelopment/addon/Addon.java
+++ src/main/java/meteordevelopment/addon/Addon.java
@@ -4,13 +4,14 @@ import meteordevelopment.addon.modules.EconomyGambler;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
-import net.minecraft.item.Items;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Addon extends MeteorAddon {
     public static final Logger LOG = LoggerFactory.getLogger("EconomyGamblerAddon");
-    public static final Category GAMBLING_CATEGORY = new Category("Gambling", Items.GOLD_INGOT.getDefaultStack());
+    
+    // Erstellt die Kategorie "Gambling" ohne Minecraft-Item-Abhängigkeit im Code
+    public static final Category GAMBLING_CATEGORY = new Category("Gambling");
 
     @Override
     public void onInitialize() {
